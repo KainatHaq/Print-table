721055c5f8a2d19ac4a46d771a50c6db43abd766
KainatHaq <kainathaq52@gmail.com>
2024-09-17 09:45:15 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,20 @@
+function printTable(){
+    let tableNum = document.getElementById('NumToPrint').value;
+    let minLenght = document.getElementById('minLenght').value;
+    let maxLenght = document.getElementById('maxLenght').value;
+
+    for(let i=minLenght; i<=maxLenght; i++){
+        document.getElementById('display-table').innerHTML += `${tableNum}x ${i} = ${ tableNum*i}<br>`
+        
+    }
+    console.log('work');
+
+}
+function refresh(){
+     document.getElementById('NumToPrint').value="";
+     document.getElementById('minLenght').value="";
+    document.getElementById('maxLenght').value="";
+    document.getElementById('display-table').innerHTML="";
+
+
+}
